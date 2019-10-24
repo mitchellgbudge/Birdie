a89c9fefdff1e3e97829980f877f1940b244ddd6
Mitchell Budge <mitchellbudge@gmail.com>
2019-10-23 18:13:33 -0600

    Configure home page base UI

---
+++ BirdieApp/BirdieApp/HomePageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomePageViewController.swift
+//  BirdieApp
+//
+//  Created by Mitchell Budge on 10/23/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    @IBOutlet weak var courseImageView: UIImageView!
+    @IBOutlet weak var newRoundButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
